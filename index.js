c38d0669009796eecf51627f467c9125d5126e58
Reggie <reganlussier@gmail.com>
2018-11-15 22:24:32 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var countDownTil = new Date('Dec 25, 2018 00:01:00').getTime();
+
+var tick =  setInterval(function() {
+  
+  var currentTime = new Date().getTime();
+  
+  var timeDifference = countDownTil - currentTime;
+  
+  var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  
+  var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+         
+   var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  
+     var seconds = Math.floor((timeDifference % (1000 * 60)) / (1000));
+  
+  document.getElementById("days").innerHTML = days;  
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+  
+}, 1000);
